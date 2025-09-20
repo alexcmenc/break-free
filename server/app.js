fe8ca708de58e140600d73095c6bb4fa1651d2b3
F4ult7 <kostantinoskazoullis@gmail.com>
2025-09-20 19:35:32 +0200

    test

---
+++ server/app.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const express = require("express");
+const { connectDB } = require("./config/connect.js");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+// const logRoutes = require("./routes/logRoutes.js");
+// const authRoutes = require("./routes/authRoutes.js");
+// const milestoneRoutes = require("./routes/milestoneRoutes.js");
+// const userRoutes = require("./routes/userRoutes.js");
+const app = express();
+
+//middleware
+app.use(
+  cors({
+    origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+//mount routes
+// app.use("/api/logs",logRoutes);
+// app.use("/api/auth", authRoutes);
+// app.use("/api/users", milestoneRoutes);
+// app.use("/api/users", userRoutes);
+
+
+app.get("/health", (_req, res) => res.json({ ok: true }));
+
+app.get("/", (_req, res) => {
+  res.send("Hello World!")
+});
+
+const PORT = process.env.PORT || 4000;
+connectDB(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`✅ API ready on http://localhost:${PORT}`)
+    );
+  })
+  .catch((err) => {
+    console.error("Server start failed:", err);
+    process.exit(1);
+  });
---
+++ server/models/User.model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const User = mongoose.model("User", userSchema);
+const addictionTypes = ["smoking", "drinking", "vaping", "sugar", "food"];
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 15,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/^\S+@\S+\.\S+$/, "Invalid email address"],
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+    quitDate: { type: Date, default: null },
+    addictionType: { type: String, enum: addictionTypes, default: "smoking" },
+  },
+  { timestamps: true }
+);
+
+module.exports(User, addictionTypes);
+
