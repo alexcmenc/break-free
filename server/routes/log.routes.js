9a12ccfe72f086e44c448fb45b24f3f7605c73d7
Alec E <alexcmenc@gmail.com>
2025-09-25 19:14:16 +0100

    nothing

--- server/routes/log.routes.js
+++ server/routes/log.routes.js
@@ -4,20 +4,10 @@ const authRequired = require("../middleware/jwt.middleware");
 
 router.use(authRequired);
 
-//list with filter / pagination
 router.get("/", logs.listLogs);
-
-//create
 router.post("/", logs.createLog);
-
-//Read one
 router.get("/:id", logs.getLog);
-
-//Update one
-
 router.patch("/:id", logs.updateLog);
-
-//Delete
 router.delete("/:id", logs.deleteLogs);
 
 module.exports = router;
