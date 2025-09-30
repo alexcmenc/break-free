8b516f028e317dd259dafee94353dff3808af185
Alec E <alexcmenc@gmail.com>
2025-09-30 19:27:23 +0100

    testing

--- server/models/User.js
+++ server/models/User.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const addictionTypes = ["smoking", "drinking", "vaping", "sugar", "food"];
 
+//userSchema.model
 const userSchema = new mongoose.Schema(
   {
     username: {
